633a07b44a41f2737393eda79d2657b8854c9ee3
IanGWhite <97561180+IanGWhite@users.noreply.github.com>
2026-02-18 17:23:36 -0600

    Add files via upload
    Adding generated code

---
+++ User.java
@@ -0,0 +1,41 @@
+public class User {
+	private int _accountNumber;
+	private int _socialSecurity;
+	private String _email;
+	private Statement _currentStatement;
+	private Statement _statementList;
+	private String _address;
+	private User _associatedAccounts;
+	private String _password;
+	public User _unnamed_User_;
+	public Client _unnamed_Client_;
+	public Statement _unnamed_Statement_;
+
+	public void changeEmail() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void viewStatement(Object aStatement_statement) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void linkAccounts() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void switchAccounts() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void login() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void searchStatements() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void downloadStatement() {
+		throw new UnsupportedOperationException();
+	}
+}
